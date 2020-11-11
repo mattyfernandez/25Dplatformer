7c0315c9c25fae279fd7163e355e48d22339a13c
Matty Fernandez <m.matiasfernandez88@gmail.com>
2020-11-11 02:22:34 -0300

    Character Idle/Move
    Control and animations.

---
+++ 25DPlatformer/Assets/Scripts/CharacterControl.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace platformer
+{
+    public enum TransitionParameter
+    {
+        Move,
+    }
+
+    public class CharacterControl : MonoBehaviour
+    {
+        public float speed;
+        public Animator animator;
+        void Update()
+        {
+            if(VirtualInputManager.Instance.MoveRight && VirtualInputManager.Instance.MoveLeft)
+            {
+                animator.SetBool(TransitionParameter.Move.ToString(), false);
+                return;
+            }
+
+            if (!VirtualInputManager.Instance.MoveRight && !VirtualInputManager.Instance.MoveLeft)
+            {
+                animator.SetBool(TransitionParameter.Move.ToString(), false);
+            }
+
+            if (VirtualInputManager.Instance.MoveRight)
+            {
+                this.gameObject.transform.Translate(Vector3.forward * speed * Time.deltaTime);
+                this.gameObject.transform.rotation = Quaternion.Euler(0f, 0f, 0f);
+                animator.SetBool(TransitionParameter.Move.ToString(), true);
+            }
+
+            if (VirtualInputManager.Instance.MoveLeft)
+            {
+                this.gameObject.transform.Translate(Vector3.forward * speed * Time.deltaTime);
+                this.gameObject.transform.rotation = Quaternion.Euler(0f, -180f, 0f);
+                animator.SetBool(TransitionParameter.Move.ToString(), true);
+            }
+        }
+    }
+
+}
