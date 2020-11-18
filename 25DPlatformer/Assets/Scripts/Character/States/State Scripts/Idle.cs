f1a7a1fbc63461ec3e03481681e145d78ac15b56
Matty Fernandez <m.matiasfernandez88@gmail.com>
2020-11-18 07:36:41 -0300

    Uppercut Combo

--- 25DPlatformer/Assets/Scripts/Character/States/State Scripts/Idle.cs
+++ 25DPlatformer/Assets/Scripts/Character/States/State Scripts/Idle.cs
@@ -10,6 +10,7 @@ namespace platformer
         public override void OnEnter(CharacterState characterState, Animator animator, AnimatorStateInfo stateInfo)
         {
             animator.SetBool(TransitionParameter.Jump.ToString(), false);
+            animator.SetBool(TransitionParameter.Attack.ToString(), false);
         }
         public override void UpdateAbility(CharacterState characterState, Animator animator, AnimatorStateInfo stateInfo)
         {
@@ -38,7 +39,7 @@ namespace platformer
 
         public override void OnExit(CharacterState characterState, Animator animator, AnimatorStateInfo stateInfo)
         {
-
+            animator.SetBool(TransitionParameter.Attack.ToString(), false);
         }
     }
 
